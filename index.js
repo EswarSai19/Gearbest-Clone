1e446b9884420b7c66ba4ff4c523b55318ad76d7
Ravi Nishad <97040275+ravinishadcoder@users.noreply.github.com>
2022-04-02 09:08:50 +0530

    add sliding img in home page

--- index.js
+++ index.js
@@ -134,3 +134,25 @@ var catdata=[
 
  
 //  top part js end here
+
+
+// slideshow js part
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  
+  setTimeout(showSlides,3000); // Change image every 2 seconds
+}
